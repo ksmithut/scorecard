46c205c7770919021afc1ab2d3e60ea68e275aa6
ksmithut <github@ksmithut.com>
2022-06-26 22:16:08 -0600

    possible service worker support?

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 const { DEV_PORT = '3000' } = process.env
 
@@ -8,7 +9,7 @@ const DESTINATION = new URL('./public/', import.meta.url).pathname
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA({})],
   root: SOURCE,
   build: {
     outDir: DESTINATION,
